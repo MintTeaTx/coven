c0ddcc2f7a69cf52fdb7775d600bf6dad251f454
Sadie Davis <32.bit.mint.tin@gmail.com>
2022-08-04 15:18:45 -0500

    big fixes

--- coven.js
+++ coven.js
@@ -3,8 +3,11 @@ const config = require('config');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const {Server} = require("socket.io");
-const io = new Server (server);
+const sanitize = require('sanitize-html');
+const {
+  Server
+} = require("socket.io");
+const io = new Server(server);
 
 let userMap = [];
 
@@ -12,93 +15,150 @@ app.set('view engine', 'ejs');
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.render('templates/template', {filename: "/chat"});
+  res.render('templates/template', {
+    filename: "/chat"
+  });
 });
 
-io.on('connection', (socket)=>{
+
+io.use((socket, next) => {
+  // const sessionID = socket.handshake.auth.sessionID;
+  // if (sessionID) {
+  //   const session = sessionStore.findSession(sessionID);
+  //   if (session) {
+  //     socket.sessionID = sessionID;
+  //     socket.userID = session.userID;
+  //     socket.username = session.username;
+  //     return next();
+  //   }
+  // }
+  const username = socket.handshake.auth.username;
+  const color = socket.handshake.auth.color;
+  if(!username){
+    return next(new Error("invalid user"));
+  }
+  if(!color) {
+    color = "#00FF00";
+  }
+  // socket.sessionID = randomId();
+  // socket.userID = randomId();
+  socket.username = sanitize(username);
+  socket.color = sanitize(color);
+  next();
+});
+
+io.on('connection', (socket) => {
+  const users = [];
+ for (let [id, socket] of io.of("/").sockets) {
+   users.push({
+     userID: id,
+     user: user(socket)
+
+   });
+ }
+ socket.emit("users", users);
+ socket.broadcast.emit("userConnected", {
+   userID: socket.id,
+   user: user(socket)
+ });
+ socket.join("general");
+ socket.join("general2");
+
   let res = {};
+    socket.emit("session", {
+      sessionID: socket.sessionID,
+      userID: socket.userID
+    });
 
-  console.log('cunt connected');
-  userMap[socket.client.id] = {
-    name:"anon",
-    color:"#FF0000"
-  };
-  console.log(JSON.stringify(userMap));
+    socket.on('joinChannel', ()=> {
 
+    });
+
+    //on client disconnect
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+      socket.emit('userDisconnected', {
+        userID: socket.id,
+        username: socket.username
+      });
+    });
+
+    socket.on('sendMessage', (json) => {
+        console.log("Message recieved");
+        console.log(json);
+        if (json.msg.length < 1) {
+          sendError(socket, "Message must be longer");
+          return;
+        }
+        io.to(json.channel).emit('sentMessage', {
+          channel: json.channel,
+          user: user(socket),
+          msg: sanitize(json.msg)
+        });
+      });
+
+      //on client changinge user
+      socket.on('changeUsername', (msg) => {
+        if (msg.length < 3) {
+          sendError(socket, "username must be at least three long");
+          return;
+        }
+        let old = socket.username;
+        res = {
+          msg: old + " has changed their name to " + msg
+        };
+        socket.username = msg;
+        sendError(socket, old + " has changed their name to " + msg);
+        io.emit('systemMessage', JSON.stringify(res));
+      });
+      //on client change color
+
+/*
   io.emit('userConnected', JSON.stringify(userMap[socket.client.id]));
   //on client disconnect
-  socket.on('disconnect', ()=>{
+  socket.on('disconnect', () => {
     console.log('user disconnected');
     console.log(userMap);
-    userMap=userMap.splice(userMap.indexOf(socket.client.id),1);
+    userMap = userMap.splice(userMap.indexOf(socket.client.id), 1);
     socket.emit('userDisconnected', JSON.stringify(userMap[socket.client.id]));
   });
   //on client sending a message
-  socket.on('sendMessage', (msg) => {
-    if(msg.length < 1)
-    {
-      sendError(socket,"Message must be longer");
-      return;
-    }
-    res = {
-        user: userMap[socket.client.id],
-        msg: msg
-    };
-    console.log(res);
-    io.emit('sentMessage',JSON.stringify(res));
-  });
-  //on client changinge user
-  socket.on('changeUsername', (msg) => {
-    if(msg.length < 3)
-    {
-      sendError(socket,"username must be at least three long");
-      return;
-    }
-    let old = userMap[socket.client.id].name;
-    userMap[socket.client.id].name = msg;
-    res = {
-        msg:  old+" has changed their name to "+msg
-    };
-    sendError(socket,old+" has changed their name to "+msg);
-    io.emit('systemMessage',JSON.stringify(res));
-  });
-  //on client change color
-  socket.on('changeColor', (msg) => {
-    if(!(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(msg)))
-    {
-          sendError(socket,"hey cheeky cunt it's a hex color");
-      return;
-    }
-    let old = userMap[socket.client.id].color;
-    userMap[socket.client.id].color = msg;
-    sendError(socket,"Successfully changed color!");
-  });
+
+
+  */
 });
 
-function sendError(socket, msg){
-  res = {
-       msg: msg
-   };
- socket.emit('errorMessage', res);
+function user(socket){
+  return {
+    name:sanitize(socket.username),
+    color:sanitize(socket.color)
+  };
 }
 
-function simpleStringify (object){
-    // stringify an object, avoiding circular structures
-    // https://stackoverflow.com/a/31557814
-    var simpleObject = {};
-    for (var prop in object ){
-        if (!object.hasOwnProperty(prop)){
-            continue;
-        }
-        if (typeof(object[prop]) == 'object'){
-            continue;
-        }
-        if (typeof(object[prop]) == 'function'){
-            continue;
-        }
-        simpleObject[prop] = object[prop];
+function sendError(socket, msg) {
+  res = {
+    msg: msg
+  };
+  socket.emit('errorMessage', res);
+}
+
+function simpleStringify(object) {
+  // stringify an object, avoiding circular structures
+  // https://stackoverflow.com/a/31557814
+  var simpleObject = {};
+  for (var prop in object) {
+    if (!object.hasOwnProperty(prop)) {
+      continue;
     }
-    return JSON.stringify(simpleObject); // returns cleaned up JSON
+    if (typeof(object[prop]) == 'object') {
+      continue;
+    }
+    if (typeof(object[prop]) == 'function') {
+      continue;
+    }
+    simpleObject[prop] = object[prop];
+  }
+  return JSON.stringify(simpleObject); // returns cleaned up JSON
 };
 
 server.listen(3000, () => {
--- public/js/coven.js
+++ public/js/coven.js
@@ -1,10 +1,23 @@
 var username = 'anon';
 
+const systemUser={
+  name:"SERVER",
+  color:"#DDAADD"
+};
 
-var socket = io();
+var socket = io({
+  autoConnect: false
+});
+
+var selectedChannel = "general";
+
+var userMap = [];
+
+let loggedIn = false;
 
 $(document).ready(function() {
 
+  initialAuth();
 
   $("#x").on({
     mouseenter: function() {
@@ -23,79 +36,175 @@ $(document).ready(function() {
     }
   });
 
-  $("#cursor").remove();
-  $("#input").focus();
-
-  $("button").on('click', function() {
-    console.log("Clicked! " + $(this).attr('id'));
-    switch ($(this).attr('id')) {
-      case 'sendMessage':
-        sendMessage();
-        break;
-      case 'setUser':
-        $("#userModal").css("display", "block");
-        break;
-      case 'submitProfile':
-        changeUsername();
-        break;
-      default:
-
+  $("#close").on({
+    mouseenter: function() {
+      $(this).css("color", "red");
+    },
+    mouseleave: function() {
+      $(this).css("color", "white");
+    },
+    click: function() {
+      hideModal();
     }
   });
 
-  socket.onAny((event, ...args) => {
+  $("#tabContainer > button").on('click',function() {
+    console.log(this.value);
+    selectedChannel = this.value;
+    $(".channel-button").removeClass('active');
+    $(".message-list").removeClass('active');
+    $("#"+this.id).addClass('active');
+    $("#"+this.value+"-messages").addClass('active');
 
-    let json = args;
+  });
 
-    console.log(event);
+  socket.on('users', (users) => {
+    console.log(users);
+    users.forEach((user) => {
+      console.log(user);
+      user.self = user.userID === socket.id;
+      userMap.push(user);
+    });
+    console.log(users);
+    userMap = users.sort((a, b) => {
+      if (a.self) return -1;
+      if (b.self) return 1;
+      if (a.username < b.username) return -1;
+      return a.username > b.username ? 1 : 0;
+    });
+
+  });
+  socket.on("sentMessage", (json) => {
+    sentMessage(json.channel, json.user, json.msg);
+  });
+  socket.on('errorMessage', (args) => {
+    sendMessage(selectedChannel,systemUser, json.msg);
+  });
+  socket.on('systemMessage', (args) => {
+    json = JSON.parse(args);
+    $("#messageList").append('<div class="entry">> <span style="color:#00FF00">SERVER</span>:' + json.msg + '</div>');
+  });
+  socket.on('profileEdit', (edit) => {
+    console.log(edit);
+  })
+  socket.on('userConnected', (json) => {
+    userMap.push(json.user);
+    let user = json.user;
+    $("#"+selectedChannel+"-messages").append('<div class="entry">><span style="color:'+user.color+';"> ' + user.name + '</span> has connected!</div>');
+  });
+  socket.on('userDisconnected', (user) => {
+    $("#messageList").append('<div class="entry">> ' + user.name + ' has disconnected!</div>');
+  });
+  socket.on('session', (args) => {
     console.log(args);
-    switch (event) {
+    // socket.auth = {args[0]};
+    //
+    // localStorage.setItem("sessionID", sessionID);
+    // socket.userID = args[1];
+  });
+  socket.on('error', (err) => {
+    json = JSON.parse(args);
 
-      case "sentMessage":
-        json = JSON.parse(args[0]);
-        $("#messageList").append('<div class="entry">> <span style="color:' + json.user.color + '">' + json.user.name + '</span>:' + json.msg + '</div>');
+    switch (json.code) {
+      case 441:
+        alert("Username Invalid");
         break;
-      case 'errorMessage':
-        json = JSON.parse(args[0]);
-        $("#messageList").append('<div class="entry" style="color:#FF0000">> SERVER</span>:' + json.msg + '</div>');
+      case 442:
+        alert("Username Taken");
         break;
-      case 'systemMessage':
-        json = JSON.parse(args[0]);
-        $("#messageList").append('<div class="entry">> <span style="color:#00FF00">SERVER</span>:' + json.msg + '</div>');
-        break;
-      case 'userConnected':
-        json = JSON.parse(args[0]);
-        $("#messageList").append('<div class="entry">> ' + json.name + ' has connected!</div>');
-        break;
-      case 'userDisconnected':
-        json = JSON.parse(args[0]);
-        $("#messageList").append('<div class="entry">> ' + json.name + ' has disconnected!</div>');
-        break;
-      default:
-
     }
   });
+
 });
 
+function sentMessage(channel, from, content) {
+  $("#"+channel+"-messages").append('<div class="entry"><span style="color:'+(from.color??'#00FF00')+ ';">' + from.name + '</span>>' + content + '</div>');
+}
 
+function showUserList() {
+  $("#userList").empty();
+  userMap.forEach((user) => {
+    $("#userList").append('<div class="entry" style="color:'+user.user.color+';">' + user.user.name + '</div>')
+  });
+  showModal("User List", 'userListCard');
+
+}
+
+function initialAuth() {
+  const sessionID = localStorage.getItem("sessionID");
+  if (sessionID) {
+    alert("Found session ID!");
+    loggedIn = true;
+    socket.auth = {
+      sessionID
+    };
+    socket.connect();
+  } else {
+    showModal("Update Profile", 'userProfileForm');
+  }
+
+}
+
+function profileSubmit() {
+  if (loggedIn) {
+    console.log("logged in, changing user");
+    changeUsername();
+  } else {
+    console.log("not logged in, creating user");
+    createUser($("#usernameField").val(), $("#colorField").val());
+  }
+}
+
+function showModal(title, elem) {
+  console.log(elem);
+  $("#modalTitle").text(title);
+  $("#" + elem).show();
+  $("#modal").show();
+}
+
+function hideModal() {
+  $("#modalTitle").text('');
+  $(".modal-content").hide();
+
+  $("#modal").hide();
+}
+
+function createUser(username, color) {
+
+  socket.auth = {
+    username,
+    color
+  };
+  socket.connect();
+  loggedIn = true;
+  hideModal();
+}
 
 function sendMessage() {
   let input = $("#messageInput").val();
-  socket.emit('sendMessage', $("#messageInput").val());
+  socket.emit('sendMessage', {msg: $("#messageInput").val(), channel:selectedChannel});
   $("#messageInput").val('');
   return true;
 }
 
 function changeUsername() {
+
   let nameInput = $("#usernameField").val();
   let colorInput = $("#colorField").val();
   if (nameInput.length > 3) {
     socket.emit('changeUsername', nameInput);
-    socket.auth = {nameInput};
   }
   if (colorInput.length > 0) {
     socket.emit('changeColor', colorInput);
   }
-  $("#userModal").hide();
+  hideModal();
   return true;
+}
+
+function joinChannel() {
+
+}
+
+function logout() {
+  socket.disconnect();
 }
